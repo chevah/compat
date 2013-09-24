d835f9c28ab9ed2043e5210e9c1a7b7aa4c77b7f
Adi Roiban <adi.roiban@chevah.com>
2013-09-24 16:20:44 +0300

    Merge with master.

--- chevah/compat/administration.py
+++ chevah/compat/administration.py
@@ -12,6 +12,9 @@ import random
 import subprocess
 import sys
 
+from chevah.empirical.testcase import (
+    ChevahTestCase,
+    )
 from chevah.compat import (
     LocalFilesystem,
     system_users,
@@ -350,6 +353,7 @@ class OSAdministration(object):
             'flags': win32netcon.UF_SCRIPT,
             'script_path': None,
         }
+
         win32net.NetUserAdd(user.pdc, 1, user_info)
         if user.password and create_profile:
             if user.domain:
@@ -441,13 +445,22 @@ class OSAdministration(object):
 
         For not this works, but this code is not 100% valid.
         """
+
+        # NetUserChangePassword works a little different that other
+        # Net* functions. In order to work on local computer it requires
+        # that the first argument be the computer name and not 'None'
+        # like the rest of Net* functions.
+        pdc = user.pdc
+        if not pdc:
+            pdc = ChevahTestCase.getHostname()
+
         try:
             import win32net
             win32net.NetUserChangePassword(
-                user.pdc, user.name, user.password, user.password)
+                pdc, user.name, user.password, user.password)
         except:
-            print 'Failed to set password "%s" for user "%s".' % (
-                user.password, user.name)
+            print 'Failed to set password "%s" for user "%s" on pdc "%s".' % (
+                user.password, user.name, pdc)
             raise
 
     def deleteUser(self, user):
