33df1bd341f3d23594fe06975f05fc30e0b2ac17
Adi Roiban <adi.roiban@chevah.com>
2013-11-29 11:08:43 +0200

    Initial fix for independent temp folder.

--- pavement.py
+++ pavement.py
@@ -14,11 +14,11 @@ if os.name == 'nt':
 
 # Marker for paver.sh.
 # This value is pavers by bash. Use a strict format.
-BRINK_VERSION = '0.24.0'
+BRINK_VERSION = '0.31.1'
 PYTHON_VERSION = '2.7'
 
 RUN_PACKAGES = [
-    'chevah-empirical==0.15.1',
+    'chevah-empirical==0.17.0',
     'zope.interface==3.8.0',
     'twisted==12.1.0-chevah3',
     ]
