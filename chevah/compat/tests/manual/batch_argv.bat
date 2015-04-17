04b3c56f11af2e9c4bf50af6badf9b787fe7cd63
Adi Roiban <adi.roiban@chevah.com>
2015-04-17 13:14:33 +0100

    Fix import.

--- chevah/compat/tests/manual/batch_argv.bat
+++ chevah/compat/tests/manual/batch_argv.bat
@@ -18,4 +18,4 @@ ECHO "Call as file"
 %python_exe% ..\chevah\compat\tests\manual\print_argv.py ^%*^
 
 ECHO "Call as module"
-%python_exe% -m chevah\compat\tests\manual\print_argv.py ^%*^
+%python_exe% -m chevah.compat.tests.manual.print_argv ^%*^
