4ab6525488342577558dd7b02a97986ebe91b63d
Adi Roiban <adi.roiban@chevah.com>
2015-04-17 12:21:23 +0100

    Update batch script.

--- chevah/compat/tests/manual/batch_argv.bat
+++ chevah/compat/tests/manual/batch_argv.bat
@@ -5,11 +5,11 @@
 CHCP 65001 > nul
 SET PYTHONIOENCODING=utf-8
 
-CD %~dp0
-if not exist lib\python.exe (
-    ECHO This file must be called from the root build folder.
+SET python_exe=lib\python.exe
+if not exist %python_exe% (
+    ECHO This must be called from the root build folder.
     PAUSE
     GOTO:EOF
 )
 
-lib\python.exe -m chevah.compat.tests.manual.print_argv ^%*^
+%python_exe% -m chevah.compat.tests.manual.print_argv ^%*^
