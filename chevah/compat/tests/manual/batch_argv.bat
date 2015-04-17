780382935855f029014a4633ea45b0992095b851
Adi Roiban <adi.roiban@chevah.com>
2015-04-17 12:58:35 +0100

    Update test.

--- chevah/compat/tests/manual/batch_argv.bat
+++ chevah/compat/tests/manual/batch_argv.bat
@@ -5,11 +5,15 @@
 CHCP 65001 > nul
 SET PYTHONIOENCODING=utf-8
 
-SET python_exe=lib\python.exe
+SET python_exe=build-windows-x86\lib\python.exe
 if not exist %python_exe% (
-    ECHO This must be called from the root build folder.
+    ECHO This must be called from the repo root folder.
     PAUSE
     GOTO:EOF
 )
 
-%python_exe% ..\chevah\compat\tests\manual\print_argv.py ^%*^
+ECHO "Call as file"
+%python_exe% chevah\compat\tests\manual\print_argv.py ^%*^
+
+ECHO "Call as module"
+%python_exe% chevah\compat\tests\manual\print_argv.py ^%*^
--- chevah/compat/tests/manual/print_argv.py
+++ chevah/compat/tests/manual/print_argv.py
@@ -8,6 +8,15 @@ Like::
     python chevah/compat/tests/manual/print_argv.py ARG1 ARG2
 """
 import sys
-from chevah import compat
-
+print 'Before import (for module import is already called)'
 print sys.argv
+
+from chevah import compat
+print 'After import'
+print sys.argv
+
+from chevah.compat.nt_unicode_argv import get_unicode_argv
+print 'Call again'
+print get_unicode_argv()
+print 'And again'
+print get_unicode_argv()
