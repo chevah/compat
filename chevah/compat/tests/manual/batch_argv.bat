a619a7431b5cde67907387cdf2f1bb2cceace6fe
Adi Roiban <adi.roiban@chevah.com>
2015-04-17 13:12:59 +0100

    Update script.

--- chevah/compat/tests/manual/batch_argv.bat
+++ chevah/compat/tests/manual/batch_argv.bat
@@ -1,19 +1,21 @@
 :: Batch command to test passing of Unicode arguments
 :: and arguments with spaces from a Windows batch file
+:: Make sure you call build command before this so that build folder is
+:: updated.
 
 @ECHO OFF
 CHCP 65001 > nul
 SET PYTHONIOENCODING=utf-8
 
-SET python_exe=build-windows-x86\lib\python.exe
+SET python_exe=lib\python.exe
 if not exist %python_exe% (
-    ECHO This must be called from the repo root folder.
+    ECHO This must be called from the root build folder.
     PAUSE
     GOTO:EOF
 )
 
 ECHO "Call as file"
-%python_exe% chevah\compat\tests\manual\print_argv.py ^%*^
+%python_exe% ..\chevah\compat\tests\manual\print_argv.py ^%*^
 
 ECHO "Call as module"
-%python_exe% chevah\compat\tests\manual\print_argv.py ^%*^
+%python_exe% -m chevah\compat\tests\manual\print_argv.py ^%*^
--- chevah/compat/tests/manual/print_argv.py
+++ chevah/compat/tests/manual/print_argv.py
@@ -6,8 +6,12 @@ Like::
 
     python -m chevah.compat.tests.manual.print_argv ARG1 ARG2
     python chevah/compat/tests/manual/print_argv.py ARG1 ARG2
+
+Make sure you call build command before calling this so that build folder
+is updated.
 """
 import sys
+import os
 print 'Before import (for module import is already called)'
 print sys.argv
 
@@ -15,8 +19,9 @@ from chevah import compat
 print 'After import'
 print sys.argv
 
-from chevah.compat.nt_unicode_argv import get_unicode_argv
-print 'Call again'
-print get_unicode_argv()
-print 'And again'
-print get_unicode_argv()
+if os.name == 'nt':
+    from chevah.compat.nt_unicode_argv import get_unicode_argv
+    print 'Call again'
+    print get_unicode_argv()
+    print 'And again'
+    print get_unicode_argv()
