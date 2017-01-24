46d6d46d652bbf8d76376b43e5d2739a3b7c3f5d
Adrian Libotean <adi.libotean@proatria.com>
2017-01-24 11:41:21 +0200

    Add conditional for skipping test based on administration privileges availability.

--- chevah/compat/tests/normal/testing/test_testcase.py
+++ chevah/compat/tests/normal/testing/test_testcase.py
@@ -617,6 +617,31 @@ class TestEmpiricalTestCase(EmpiricalTestCase):
                 'is True.'
                 )
 
+    @conditionals.onAdminPrivileges(True)
+    def test_onAdminPrivileges_present(self):
+        """
+        Run test only on machines that execute the tests with administrator
+        privileges.
+        """
+        if 'win-2003' in self.hostname:
+            raise AssertionError(
+                'Windows 2003 BS does not run as administrator')
+        elif 'win-xp' in self.hostname:
+            raise AssertionError('Windows XP BS does not run as administrator')
+
+    @conditionals.onAdminPrivileges(False)
+    def test_onAdminPrivileges_missing(self):
+        """
+        Run test on build slaves that do not have administrator privileges.
+        """
+        if 'win-2003' in self.hostname:
+            return
+        elif 'win-xp' in self.hostname:
+            return
+
+        raise AssertionError(
+            '"%s" is running with administrator privileges' % (self.hostname,))
+
     def test_cleanup_test_segments_file(self):
         """
         When self.test_segments is defined it will be automatically
