0d4634eb86f1ba3475cbf96573e163a15426d709
Bruno Gola <brunogola@gmail.com>
2017-12-19 22:38:38 +0100

    add tests for the excepted_threads

--- chevah/compat/tests/normal/test_testing.py
+++ chevah/compat/tests/normal/test_testing.py
@@ -7,7 +7,11 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 from six import text_type
+from threading import Thread, Event
+
 from chevah.compat.testing import ChevahTestCase, mk
+
+import time
 
 
 class TestFactory(ChevahTestCase):
@@ -22,3 +26,53 @@ class TestFactory(ChevahTestCase):
         avatar = mk.makeFilesystemApplicationAvatar()
         self.assertIsInstance(avatar.name, text_type)
         self.assertIsInstance(avatar.home_folder_path, text_type)
+
+    def test_tearDown_excepted_threads(self):
+        """
+        Will not fail if a thread name is in excepted_threads.
+        """
+        event = Event()
+
+        class TestThread(Thread):
+            def run(self):
+                event.wait()
+
+        old_excepted_threads = self.excepted_threads[:]
+        self.excepted_threads = self.excepted_threads + ['TestThread']
+
+        thread = TestThread(name="TestThread")
+        thread.start()
+
+        self.tearDown()
+
+        self.assertTrue(thread.is_alive())
+        event.set()
+
+        # Wait for the thread to stop.
+        time.sleep(0.1)
+        self.assertFalse(thread.is_alive())
+
+        self.excepted_threads = old_excepted_threads[:]
+
+    def test_tearDown_not_excepted_threads(self):
+        """
+        Will raise AssertioError fail if a thread name is not in
+        excepted_threads.
+        """
+        event = Event()
+
+        class TestThread(Thread):
+            def run(self):
+                event.wait()
+
+        thread = TestThread(name="TestThread")
+        thread.start()
+
+        with self.assertRaises(AssertionError) as context:
+            self.tearDown()
+
+        self.assertContains('TestThread', context.exception.message)
+
+        # Stop the thread.
+        event.set()
+        time.sleep(0.1)
