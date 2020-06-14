9db0a6e93e2408e0808411d0bb0dd987789b18d8
Adi Roiban <adi.roiban@chevah.com>
2020-06-14 07:23:30 +0100

    Fix py3 linter.

--- chevah/compat/helpers.py
+++ chevah/compat/helpers.py
@@ -6,6 +6,14 @@ Any methods from here is a sign of bad design.
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import sys
+
+if sys.version_info[0] == 3:
+    unicode_type = str
+else:
+    unicode_type = unicode  # pylint: disable=unicode-builtin
 
 
 def _(string):
@@ -35,28 +43,28 @@ def force_unicode(value):
     are replaced.
     """
     if value is None:
-        return unicode(value)
+        return u'None'
 
-    elif isinstance(value, unicode):
+    elif isinstance(value, unicode_type):
         return value
 
     def str_or_repr(value):
         try:
-            return unicode(value)
+            return unicode_type(value)
         except Exception:
             # No unicode representation.
             # Try to decode the str()
             try:
-                return unicode(str(value), encoding='utf-8', errors='replace')
+                return unicode_type(str(value), encoding='utf-8', errors='replace')
             except Exception:
                 # No luck with str, try repr()
-                return unicode(repr(value), encoding='utf-8', errors='replace')
+                return unicode_type(repr(value), encoding='utf-8', errors='replace')
 
     result = str_or_repr(value)
 
     if isinstance(value, Exception) and result == '':
         # This is an exception without text representation.
         # Use the repr so that we get something.
-        return unicode(repr(value), encoding='utf-8', errors='replace')
+        return unicode_type(repr(value), encoding='utf-8', errors='replace')
 
     return result
