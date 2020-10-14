e7db37e16d5d203c5a5057e13d2ea82cea20fe26
dumol <dumol@gnome.org>
2020-10-14 12:56:42 +0300

    Updated brink from chevah/pythia.

--- brink.sh
+++ brink.sh
@@ -546,12 +546,12 @@ install_dependencies(){
 # Check version of current OS to see if it is supported.
 # If it's too old, exit with a nice informative message.
 # If it's supported, return through eval the version numbers to be used for
-# naming the package, for example '5' for RHEL 5.x, '1204' for Ubuntu 12.04',
-# '53' for AIX 5.3.x.x , '10' for Solaris 10 or '1010' for OS X 10.10.1.
+# naming the package, for example: '7' for RHEL 7.7, '2' for Amazon 2,
+# '2004' for Ubuntu 20.04', '312' for Alpine Linux 3.12, '11' for Solaris 11.
 #
 check_os_version() {
     # First parameter should be the human-readable name for the current OS.
-    # For example: "Red Hat Enterprise Linux" for RHEL, "OS X" for Darwin etc.
+    # For example: "Red Hat Enterprise Linux" for RHEL, "macOS" for Darwin etc.
     # Second and third parameters must be strings composed of integers
     # delimited with dots, representing, in order, the oldest version
     # supported for the current OS and the current detected version.
@@ -749,7 +749,7 @@ detect_os() {
                         ;;
                     alpine)
                         os_version_raw="$VERSION_ID"
-                        check_os_version "$distro_fancy_name" 3.6 \
+                        check_os_version "$distro_fancy_name" 3.12 \
                             "$os_version_raw" os_version_chevah
                         set_os_if_not_generic "alpine" $os_version_chevah
                         ;;
@@ -779,54 +779,24 @@ detect_os() {
         OpenBSD)
             ARCH=$(uname -m)
             os_version_raw=$(uname -r)
-            check_os_version "OpenBSD" 6.5 "$os_version_raw" os_version_chevah
+            check_os_version "OpenBSD" 6.7 "$os_version_raw" os_version_chevah
             OS="obsd${os_version_chevah}"
             ;;
         SunOS)
             ARCH=$(isainfo -n)
             os_version_raw=$(uname -r | cut -d'.' -f2)
-            check_os_version Solaris 10 "$os_version_raw" os_version_chevah
+            check_os_version "Solaris" 11 "$os_version_raw" os_version_chevah
             OS="sol${os_version_chevah}"
-            case "$OS" in
-                sol10)
-                    # Solaris 10u8 (from 10/09) updated libc version, so for
-                    # older releases up to 10u7 (from 5/09) we build on 10u3.
-                    # The "sol10u3" code path also shows the way to link to
-                    # OpenSSL 0.9.7 libs bundled in /usr/sfw/ with Solaris 10.
-                    # Update number is taken from first line of /etc/release.
-                    un=$(head -1 /etc/release | cut -d_ -f2 | sed s/[^0-9]*//g)
-                    if [ "$un" -lt 8 ]; then
-                        OS="sol10u3"
-                    fi
-                    ;;
-                sol11)
-                    # Solaris 11 releases prior to 11.4 bundled OpenSSL libs
-                    # missing support for Elliptic-curve crypto. From here on:
-                    #   * Solaris 11.4 (or newer) with OpenSSL 1.0.2 is "sol11",
-                    #   * Solaris 11.2/11.3 with OpenSSL 1.0.1 is "sol112",
-                    #   * Solaris 11.0/11.1 with OpenSSL 1.0.0 is not supported.
-                    minor_version=$(uname -v | cut -d'.' -f2)
-                    if [ "$minor_version" -lt 4 ]; then
-                        OS="sol112"
-                    fi
-                    ;;
-            esac
-            ;;
-        AIX)
-            ARCH="ppc$(getconf HARDWARE_BITMODE)"
-            os_version_raw=$(oslevel)
-            check_os_version AIX 5.3 "$os_version_raw" os_version_chevah
-            OS="aix${os_version_chevah}"
-            ;;
-        HP-UX)
-            ARCH=$(uname -m)
-            os_version_raw=$(uname -r | cut -d'.' -f2-)
-            check_os_version HP-UX 11.31 "$os_version_raw" os_version_chevah
-            OS="hpux${os_version_chevah}"
+            check_os_version Solaris 11 "$os_version_raw" os_version_chevah
+            if [ "$minor_version" -lt 4 ]; then
+                (>&2 echo "Unsupported Solaris 11 version, 11.4 needed.")
+                exit 15
+            fi
             ;;
         *)
             (>&2 echo "Unsupported operating system: ${OS}.")
             exit 14
+            ;;
     esac
 
     # Normalize arch names. Force 32bit builds on some OS'es.
@@ -837,16 +807,12 @@ detect_os() {
         "amd64"|"x86_64")
             ARCH="x64"
             case "$OS" in
-                sol10*)
-                    # On Solaris 10, x64 built fine prior to adding "bcrypt".
-                    ARCH="x86"
-                    ;;
                 win)
                     # 32bit build on Windows 2016, 64bit otherwise.
                     # Should work with a l10n pack too (tested with French).
                     win_ver=$(systeminfo.exe | head -n 3 | tail -n 1 \
-                        | cut -d ":" -f 2 | cut -b 15-43)
-                    if [ "$win_ver" = "Microsoft Windows Server 2016" ]; then
+                        | cut -d ":" -f 2)
+                    if [[ "$win_ver" =~ "Microsoft Windows Server 2016" ]]; then
                         ARCH="x86"
                     fi
                     ;;
@@ -854,15 +820,6 @@ detect_os() {
             ;;
         "aarch64")
             ARCH="arm64"
-            ;;
-        "ppc64")
-            # Python has not been fully tested on AIX when compiled as a 64bit
-            # binary, and has math rounding error problems (at least with XL C).
-            ARCH="ppc"
-            ;;
-        "sparcv9")
-            # We build 32bit binaries on SPARC too. Use "sparc64" for 64bit.
-            ARCH="sparc"
             ;;
     esac
 }
