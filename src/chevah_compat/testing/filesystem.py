6cda862d1ce1b422e90a056919aa929d5dbbdc63
Adi Roiban <adi.roiban@chevah.com>
2023-05-23 19:03:24 +0100

    Update after merge with master.

--- src/chevah_compat/testing/filesystem.py
+++ src/chevah_compat/testing/filesystem.py
@@ -7,7 +7,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 import six
-import hashlib
 import os
 import re
 import uuid
--- src/chevah_compat/testing/testcase.py
+++ src/chevah_compat/testing/testcase.py
@@ -8,7 +8,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 import six
-from six.moves import range
 import contextlib
 import inspect
 import threading
