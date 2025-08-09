1acd7ab9fca5439cd39959e5faecd40ed9cf424e
Adi Roiban <adiroiban@gmail.com>
2025-08-09 15:20:20 +0100

    Help troubleshoot windows.

--- src/chevah_compat/administration.py
+++ src/chevah_compat/administration.py
@@ -891,7 +891,7 @@ class OSAdministrationWindows(OSAdministrationUnix):
             'flags': win32netcon.UF_SCRIPT,
             'script_path': None,
         }
-
+        print(f"Setting password for f{user.name}")
         win32net.NetUserAdd(user.pdc, 1, user_info)
         if user.windows_create_local_profile:
             if not user.password:  # pragma: no cover
